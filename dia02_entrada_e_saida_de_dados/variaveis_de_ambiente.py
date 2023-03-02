b130d84121e54ee19a202a9743929ad5d2a601ac
caiooliveirah09 <leelros@hotmail.com>
2023-03-02 15:31:42 -0300

    add environment variables, and remove the user and create pokemon

--- dia02_entrada_e_saida_de_dados/variaveis_de_ambiente.py
+++ dia02_entrada_e_saida_de_dados/variaveis_de_ambiente.py
@@ -1,7 +1,8 @@
 from decouple import config
 
-API_USER = config("USER")
+API_POKEMON = config("POKEMON")
 API_KEY = config("KEY")
 DEBUG_MODE = config("DEBUG_MODE", default=False, cast=bool)
 
-print(API_USER)
+print(API_POKEMON)
+print(API_KEY)
